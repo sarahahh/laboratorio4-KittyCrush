04ab1c5257a348491680692dedbed7e5b908c837
Manuel Sepulveda <manuelsegaviria@gmail.com>
2023-11-06 13:22:54 -0500

    mas funciones

--- KittyCrush/Tablero.cs
+++ KittyCrush/Tablero.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace KittyCrush
@@ -11,11 +12,14 @@ namespace KittyCrush
     internal class Tablero
     {
         //Funciones getters, setters y valores de matriz
-        public int cantFilas {  get; set; }
+        public int cantFilas { get; set; }
         public int cantColumnas { get; set; }
         public int[,] valores { get; set; }
         public bool[,] bools { get; set; }
-        public int contador = 0;
+        public int contador { get; set; } = 0;
+        public bool valido { get; set; }
+        public int puntos { get; set; } = 0;
+
 
         //Constructor de la clase tablero 
         public Tablero(int cantFilas, int cantColumnas, int cantKittys)
@@ -68,34 +72,103 @@ namespace KittyCrush
 
         public void comprobarVertical()
         {
-            for(int i = 0; i<valores.GetLength(0); i++)
+            if(contador >= 3)
             {
-                for(int j = 0; j < valores.GetLength(1); j++)
+                for (int i = 0; i < valores.GetLength(0); i++)
                 {
-                    if (bools[i, j] == true)
+                    for (int j = 0; j < valores.GetLength(1); j++)
                     {
-                        if (bools[i, j+1]== true)
+                        if (bools[i, j] == true)
                         {
-                            bool vertical = false;
-                            comprobar(vertical, i, j);
-                        }
-                        else if (bools[i+1, j]== true)
-                        {
-                            bool vertical = true;
-                            comprobar(vertical, i, j);
-                        }
-                        else
-                        {
-                            break;
+                            if (bools[i, j + 1] == true)
+                            {
+                                bool vertical = false;
+                                comprobar(vertical, i, j);
+                            }
+                            else if (bools[i + 1, j] == true)
+                            {
+                                bool vertical = true;
+                                comprobar(vertical, i, j);
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
                     }
                 }
+            }
+            else
+            {
+                agregarPuntos(-5);
             }
         }
 
         public void comprobar(bool vertical, int filaInicio, int columnaInicio)
         {
+            valido = true;
+            if (vertical==true)
+            {
+                for(int i = 0; i < valores.GetLength(0); i++)
+                {
+                    for ( int j = 0;j < valores.GetLength(1); j++)
+                    {
+                        if (bools[i, j] == true)
+                        {
+                            if (!(valores[i, j] == valores[filaInicio, columnaInicio] && j == columnaInicio))
+                            {
+                                valido = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if(vertical==false) 
+            {
+                for (int i = 0; i < valores.GetLength(0); i++)
+                {
+                    for (int j = 0; j < valores.GetLength(1); j++)
+                    {
+                        if (bools[i, j] == true)
+                        {
+                            if (!(valores[i, j] == valores[filaInicio, columnaInicio] && i == filaInicio))
+                            {
+                                valido = false;
+                            }
+                        }
+                    }
+                }
+            } 
+            if(valido == true)
+            {
+                switch (contador)
+                {
+                    case 3:
+                        agregarPuntos(6);
+                        break;
+                    case 4:
+                        agregarPuntos(10);
+                        break;
+                    case 5:
+                        agregarPuntos(15);
+                        break;
+                    default:
+                        agregarPuntos(-5);
+                        break;
+                }
+            }
+            else
+            {
+                agregarPuntos(-5);
+            }
+            contador = 0;
+            MessageBox.Show(puntos.ToString());
+            inizializarBools();
+        }
 
+        public void agregarPuntos(int pum)
+        {
+            puntos += pum;
         }
 
         public void posicion(int fila, int columna)
